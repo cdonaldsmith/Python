17f910eeb136b4be76801669434b3bf6e12fa5fd
cdonaldsmith <98066307+cdonaldsmith@users.noreply.github.com>
2022-01-23 22:33:00 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+#Print command
+print('Don Smith')
+print('o----')
+print(' ||||')
+print('*' * 10)
+
+#Variables - Int, Float, Str, Bool
+price = 10
+rating = 4.9
+name = 'Don'
+is_published = False
+print(price,rating,name)
+
+#Exercise 1
+patient_name = 'John Smith'
+patient_age = 20
+is_new_patient = True
+
+#Getting Input
+name = input('What is your name? ')
+print('Hi ' + name)
+color = input('What is your favorite color? ')
+print(name + ' likes ' + color)
